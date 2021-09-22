e422948cfba96a598b7e14754383dd0963682c88
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-09-22 08:26:46 +0530

    added the setup of the project.

---
+++ src/main/java/com/bridgelabz/AddressBookApp/WorkShopAddressBookApiApplication.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.AddressBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class WorkShopAddressBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopAddressBookApiApplication.class, args);
+		log.info("Welcome to AddressBook spring boot application.");
+		
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/AddressBookApp/WorkShopAddressBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.AddressBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopAddressBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
